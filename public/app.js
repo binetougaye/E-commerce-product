afe789ab510f7646e354af265c54c34d32a8ae12
binetougaye <fabigaye3@gmail.com>
2025-02-13 15:03:34 +0000

    feat: ajout au panier

--- public/app.js
+++ public/app.js
@@ -5,22 +5,59 @@ const removeItem = document.getElementById("minus")
 const button = document.getElementById("btn-add")
 const panier = document.querySelector(".item")
 const cartContainer = document.querySelector(".cart-container")
+const cartContent = document.getElementById("cartContent")
 // LOGIC
 let cartItem = 0
+// Valeur dans le bouton plus et moins
 cartItems.textContent = cartItem
+// Valeur dans l'icone panier
 panier.textContent = cartItem
-function addToCart() {
+cartContent.textContent = 'Your cart is empty'
+
+// Ajouter
+function addToCart(e) {
     cartItem += 1
     cartItems.textContent = cartItem
     panier.textContent = cartItem
+    if (cartItem > 0) {
+        cartContent.innerHTML = `
+
+        <div>
+            <img src='./images/image-product-1-thumbnail.jpg'/>
+         
+        </div>
+        <div>
+           <p>Fall Limited Edition Sneakers</p>
+        <p>$125.00 *   ${cartItem} <span class='font-bold'>$${125.00 * cartItem}</span></p>
+        </div>
+        
+        
+
+        `
+    } else {
+        cartContent.innerHTML = `<div>Your cart is empty</div>`
+    }
+
+
 }
+// Diminuer
 function removeToCart() {
-    cartItem -= 1
-    cartItems.textContent = cartItem
-    panier.textContent = cartItem
+    if (cartItem > 0) {
+        cartItem -= 1;
+        updateCartDisplay();
+    }
 
 
 }
+
+// Mettre à jour l'affichage du panier
+function updateCartDisplay() {
+    cartItems.textContent = cartItem;
+    panier.textContent = cartItem;
+
+    // cartContainer.textContent = ? "Your cart is filled" : "Your cart is empty";
+}
+// 
 cart.addEventListener("mouseover", () => {
     cartContainer.classList.remove("hidden")
     cartContainer.classList.add('visible')
@@ -29,6 +66,7 @@ cart.addEventListener("mouseout", () => {
     cartContainer.classList.remove("visible")
     cartContainer.classList.add('hidden')
 })
+// 
 addItem.addEventListener("click", addToCart)
 removeItem.addEventListener('click', removeToCart)
 button.addEventListener('click', addToCart)
